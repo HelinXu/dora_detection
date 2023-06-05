8d689d7f6f80d7a22e3b51e177bc92b59e05afd8
HelinXu <xuhelin1911@gmail.com>
2023-06-06 00:31:14 +0800

    [update]

--- detection-beam/app.py
+++ detection-beam/app.py
@@ -7,17 +7,19 @@ app = beam.App(
     name="dora-detection-1",
     cpu=1,
     memory="16Gi",
-    gpu=1, # TODO
-    python_version="3.10",
+    # gpu=1, # TODO
+    python_version="python3.10",
     python_packages=[
-        "torch==1.10.0",
-        "torchvision==0.11.1",
-        "detectron2==0.6",
-        "icecream==2.1.3",
-        "opencv-python==4.7.0.72",
-        "pycocotools==2.0.6",
+    ],
+    commands=[
+        "pip install torch torchvision icecream==2.1.3 opencv-python pycocotools",
+        "python -m pip install 'git+https://github.com/facebookresearch/detectron2.git'",
+        "apt-get update && apt-get install ffmpeg libsm6 libxext6  -y"
     ]
 )
+
+# app.py
+app.Mount.PersistentVolume(name="pvc", path="./pvc")
 
 # Triggers determine how your app is deployed
 app.Trigger.RestAPI(
