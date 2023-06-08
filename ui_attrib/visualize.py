189bdb240375c06bb9a3564bbfbadaf27d20763c
HelinXu <xuhelin1911@gmail.com>
2023-06-08 17:30:24 +0800

    [update]

--- ui_attrib/visualize.py
+++ ui_attrib/visualize.py
@@ -44,22 +44,31 @@ for i in range(1,100):
 
     # Retrieve the predicted labels
     predicted_fontsize = output[0, 0].item() * 300
-    predicted_fontweight = output[0, 1].item() * 1000
+    predicted_fontweight = output[0, 1].item() * 10
     predicted_color_r = output[0, 2].item() * 300
     predicted_color_g = output[0, 3].item() * 300
     predicted_color_b = output[0, 4].item() * 300
-    predicted_color_a = output[0, 5].item() * 300
 
 
     # paint the color along with the image
 
     # pred = cv2.copyMakeBorder(cv2.imread(image_path) , 10, 10, 10, 10, cv2.BORDER_CONSTANT, value=(predicted_color_b, predicted_color_g, predicted_color_r))
-    image = cv2.imread(image_path.replace('.png', '.png'))
-    # overlay the a rectangle with (RGBA) color (no fill)
-    overlay = image.copy()
-    cv2.rectangle(overlay, (0, 0), (image.shape[1], image.shape[0]), (predicted_color_b, predicted_color_g, predicted_color_r), 20)
+    image = cv2.imread(image_path.replace('.png', '-original.png'))
+    predicted_fontsize = min(predicted_fontsize, image.shape[0])
+    # the rectangle height is predicted_fontsize (in pixels), centered at the image
+    # center
+    y1 = int(image.shape[0] / 2 - predicted_fontsize / 2)
+    y2 = int(image.shape[0] / 2 + predicted_fontsize / 2)
+    x1 = int(10)
+    x2 = int(image.shape[1] - 10)
+    pred = cv2.rectangle(image, (x1, y1), (x2, y2), (predicted_color_b, predicted_color_g, predicted_color_r), int(predicted_fontweight))
     # overlay the rectangle onto our original image
-    pred = cv2.addWeighted(overlay, predicted_color_a / 256, image, 1 - predicted_color_a / 256, 0)
+
+
+    gt_image = cv2.imread(image_path)
+    # concat horizontally
+    pred = np.concatenate((pred, gt_image), axis=1)
+    
     # save
     cv2.imwrite(f'out/pred{i}.png', pred)
     
