4d2874190d1e65f98eebf7605d128852cb11a475
HelinXu <xuhelin1911@gmail.com>
2023-06-09 01:38:21 +0800

    [update] visualization debug

--- ui_attrib/visualize.py
+++ ui_attrib/visualize.py
@@ -37,13 +37,14 @@ def run_one(image, save_path, w, h, gt_dic=None):
 
 
     # Retrieve the predicted labels
+    boarder_size = int(0.2 * sqrt(w*h))
     output = output.cpu()
     predicted_fontsize = output[0, 0].item() * w
     predicted_fontweight = output[0, 1].item() * sqrt(w*h) * 150
     predicted_color_r = output[0, 2].item() * 300
     predicted_color_g = output[0, 3].item() * 300
     predicted_color_b = output[0, 4].item() * 300
-    pred_image = cv2.copyMakeBorder(image, 10, 10, 10, 10, cv2.BORDER_CONSTANT, value=(int(predicted_color_b), int(predicted_color_g), int(predicted_color_r)))
+    pred_image = cv2.copyMakeBorder(image, boarder_size, boarder_size, boarder_size, boarder_size, cv2.BORDER_CONSTANT, value=(int(predicted_color_b), int(predicted_color_g), int(predicted_color_r)))
 
     if gt_dic is not None:
         gt_fontsize = gt_dic['fontsize'] * w
@@ -57,10 +58,17 @@ def run_one(image, save_path, w, h, gt_dic=None):
         print('color_r: ', gt_color_r, predicted_color_r)
         print('color_g: ', gt_color_g, predicted_color_g)
         print('color_b: ', gt_color_b, predicted_color_b)
+
+
         # write the ground truth on a new image
-        gt_image = cv2.copyMakeBorder(image, 10, 10, 10, 10, cv2.BORDER_CONSTANT, value=(int(gt_color_b), int(gt_color_g), int(gt_color_r)))
+        gt_image = cv2.copyMakeBorder(image, boarder_size, boarder_size, boarder_size, boarder_size, cv2.BORDER_CONSTANT, value=(int(gt_color_b), int(gt_color_g), int(gt_color_r)))
         # concat
         pred_image = np.concatenate((pred_image, gt_image), axis=1)
+
+        # add border below
+        pred_image = cv2.copyMakeBorder(pred_image, 0, 40, 0, 0, cv2.BORDER_CONSTANT, value=(0, 0, 0))
+        # write the ground truth on the border
+        cv2.putText(pred_image, f'size error: {abs(gt_fontsize - predicted_fontsize)/(1+gt_fontsize)*100:.1f}%  weight err: {abs(gt_fontweight-predicted_fontweight)/(gt_fontweight+1)*100:.1f}%', (10, pred_image.shape[0]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
     
     # save the image
     cv2.imwrite(save_path, pred_image)
@@ -79,11 +87,11 @@ for i in range(20):
 
 
 # dataset
-testingset = ImageRegressionDataset(root_dir='/root/autodl-tmp/dora_font/', annotation_file='/root/autodl-tmp/dora_font/train/train.txt',
+testingset = ImageRegressionDataset(root_dir='/root/autodl-tmp/dora_font/', annotation_file='/root/autodl-tmp/dora_font/test/test.txt',
                                         transform=transforms.ToTensor())
 
-# Prepare the image for inference
-for i in range(len(testingset)):
-    image, w, h, fontsize, fontweight, color_r, color_g, color_b = testingset[i]
+# Prepare the image for inference random choice
+for i in range(100):
+    image, w, h, fontsize, fontweight, color_r, color_g, color_b = testingset[np.random.randint(0, len(testingset))]
 
-    run_one(image, f'./out/sim{i}.jpg', w, h, gt_dic={'fontsize': fontsize, 'fontweight': fontweight, 'color_r': color_r, 'color_g': color_g, 'color_b': color_b})
+    run_one(image, f'./out/sim-test{i}.jpg', w, h, gt_dic={'fontsize': fontsize, 'fontweight': fontweight, 'color_r': color_r, 'color_g': color_g, 'color_b': color_b})
