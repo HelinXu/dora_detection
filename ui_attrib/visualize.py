88470ae0b7dd168954aeb3159d9620ea36f786d7
HelinXu <xuhelin1911@gmail.com>
2023-06-09 01:08:20 +0800

    [update] visualization debug

--- ui_attrib/visualize.py
+++ ui_attrib/visualize.py
@@ -5,7 +5,8 @@ import numpy as np
 from PIL import Image
 from torchvision import transforms
 from model import RegressionResNet
-
+from dataset import ImageRegressionDataset
+from math import sqrt
 
 # Load the trained model
 model = RegressionResNet(out_channels=5)
@@ -14,61 +15,75 @@ model.eval()
 model.to('cuda')
 
 
-def run_one(image_path, save_path, gt=None):
-    image = Image.open(image_path).convert('RGB')
-    w, h = image.size
-    transform = transforms.ToTensor()
-    image = transform(image)
-    image = image.unsqueeze(0).to('cuda')  # Add batch dimension
+def run_one(image, save_path, w, h, gt_dic=None):
+    # image shape 224*224
+    inimage = image.unsqueeze(0).to('cuda')  # Add batch dimension
 
     tic = time.time()
     # Perform inference
     with torch.no_grad():
-        output = model(image, torch.tensor(w).unsqueeze(0).to('cuda'), torch.tensor(h).unsqueeze(0).to('cuda'))
+        output = model(inimage)
 
     toc = time.time()
 
     print('time: ', toc - tic)
 
+    np_image = image.permute(1, 2, 0).mul(255).byte().cpu().numpy()
+    # bgr - rgb
+    np_image = np_image[:, :, ::-1].copy()
+    
+    # Resize the image to the original dimensions (w, h)
+    image = cv2.resize(np_image, (w, h))
+
+
     # Retrieve the predicted labels
     output = output.cpu()
-    predicted_fontsize = output[0, 0].item() * 300
-    predicted_fontweight = output[0, 1].item() * 10
+    predicted_fontsize = output[0, 0].item() * w
+    predicted_fontweight = output[0, 1].item() * sqrt(w*h) * 150
     predicted_color_r = output[0, 2].item() * 300
     predicted_color_g = output[0, 3].item() * 300
     predicted_color_b = output[0, 4].item() * 300
+    pred_image = cv2.copyMakeBorder(image, 10, 10, 10, 10, cv2.BORDER_CONSTANT, value=(int(predicted_color_b), int(predicted_color_g), int(predicted_color_r)))
 
+    if gt_dic is not None:
+        gt_fontsize = gt_dic['fontsize'] * w
+        gt_fontweight = gt_dic['fontweight'] * sqrt(w*h) * 150
+        gt_color_r = gt_dic['color_r'] * 300
+        gt_color_g = gt_dic['color_g'] * 300
+        gt_color_b = gt_dic['color_b'] * 300
 
-    # paint the color along with the image
-
-    # pred = cv2.copyMakeBorder(cv2.imread(image_path) , 10, 10, 10, 10, cv2.BORDER_CONSTANT, value=(predicted_color_b, predicted_color_g, predicted_color_r))
-    image = cv2.imread(image_path.replace('.png', '-original.png'))
-    predicted_fontsize = min(predicted_fontsize, image.shape[0])
-    # the rectangle height is predicted_fontsize (in pixels), centered at the image
-    # center
-    y1 = int(image.shape[0] / 2 - predicted_fontsize / 2)
-    y2 = int(image.shape[0] / 2 + predicted_fontsize / 2)
-    x1 = int(10)
-    x2 = int(image.shape[1] - 10)
-    pred = cv2.rectangle(image, (x1, y1), (x2, y2), (predicted_color_b, predicted_color_g, predicted_color_r), int(predicted_fontweight))
-    # overlay the rectangle onto our original image
-
-
-    gt_image = cv2.imread(image_path)
-    # concat horizontally
-    pred = np.concatenate((pred, gt_image), axis=1)
+        print('fontsize: ', gt_fontsize, predicted_fontsize)
+        print('fontweight: ', gt_fontweight, predicted_fontweight)
+        print('color_r: ', gt_color_r, predicted_color_r)
+        print('color_g: ', gt_color_g, predicted_color_g)
+        print('color_b: ', gt_color_b, predicted_color_b)
+        # write the ground truth on a new image
+        gt_image = cv2.copyMakeBorder(image, 10, 10, 10, 10, cv2.BORDER_CONSTANT, value=(int(gt_color_b), int(gt_color_g), int(gt_color_r)))
+        # concat
+        pred_image = np.concatenate((pred_image, gt_image), axis=1)
     
-    # save
-    cv2.imwrite(save_path, pred)
+    # save the image
+    cv2.imwrite(save_path, pred_image)
+
 
 # Prepare the image for inference
-for i in range(1,20):
-    image_path = f'./img/{i}.jpg'  # Replace with the path to your image
-    save_path = f'./out/real{i}.jpg'
-    run_one(image_path, save_path)
+for i in range(20):
+    image = Image.open(f'./img/{i}.jpg').convert('RGB')
+    w, h = image.size
+    # resize the image to be 224x224
+    image = image.resize((224, 224))
+    # Apply transformations if provided
+    image = transforms.ToTensor()(image)
+    run_one(image, f'./out/real{i}.jpg', w, h)
+
+
+
+# dataset
+testingset = ImageRegressionDataset(root_dir='/root/autodl-tmp/dora_font/', annotation_file='/root/autodl-tmp/dora_font/train/train.txt',
+                                        transform=transforms.ToTensor())
 
 # Prepare the image for inference
-for i in range(1,100):
-    image_path = f'/root/autodl-tmp/dora_font/test/{i}.png'  # Replace with the path to your image
-    save_path = f'./out/sim{i}.png'
-    run_one(image_path, save_path)
+for i in range(len(testingset)):
+    image, w, h, fontsize, fontweight, color_r, color_g, color_b = testingset[i]
+
+    run_one(image, f'./out/sim{i}.jpg', w, h, gt_dic={'fontsize': fontsize, 'fontweight': fontweight, 'color_r': color_r, 'color_g': color_g, 'color_b': color_b})
