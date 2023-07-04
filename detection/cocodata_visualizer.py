e475561e58fd59f0d37bf27a3e9652a84cc52944
HelinXu <xuhelin1911@gmail.com>
2023-07-04 18:19:34 +0800

    [update] visualizer

--- detection/cocodata_visualizer.py
+++ detection/cocodata_visualizer.py
@@ -3,18 +3,18 @@ from detectron2.utils.visualizer import Visualizer
 import cv2
 import random
 
-num_to_visualize = 30
+num_to_visualize = 1000
 
 # Define the dataset and metadata
-dataset_name = "dora_train"
+dataset_name = "dora_sim_train"
 
 # Register the dataset
-datasets.register_coco_instances("dora_train", {}, # dataset name
-                                f"/root/autodl-tmp/dora_dataset/train.json", # path to the json file
-                                f"/root/autodl-tmp/dora_dataset/train")  # folder to the images.
-datasets.register_coco_instances("dora_val", {},
-                                f"/root/autodl-tmp/dora_dataset/val.json",
-                                f"/root/autodl-tmp/dora_dataset/val")
+datasets.register_coco_instances("dora_sim_train", {}, # dataset name
+                                f"/root/autodl-tmp/dora_sim/train/train.json", # path to the json file
+                                f"/root/autodl-tmp/dora_sim/train")  # folder to the images.
+datasets.register_coco_instances("dora_sim_test", {},
+                                f"/root/autodl-tmp/dora_sim/test/test.json",
+                                f"/root/autodl-tmp/dora_sim/test")
 metadata = MetadataCatalog.get(dataset_name)
 
 dataset_dicts = DatasetCatalog.get(dataset_name)
